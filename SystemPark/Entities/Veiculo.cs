abfef6e34d21721c6e1c82346150f480e209ca0f
lcsvinhas <l.vinhas.lv@gmail.com>
2025-07-30 20:51:11 -0300

    Criei entidade Veiculo

---
+++ SystemPark/Entities/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿namespace SystemPark.Entities
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public bool NoEstacionamento { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor {  get; set; }
+        public string Placa { get; set; }
+        public DateTime HoraEntrada { get; set; }
+        public DateTime? HoraSaida { get; set; }
+        public decimal? Valor {  get; set; }
+    }
+}
